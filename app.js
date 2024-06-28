e7268187315f7e66fdf175c49e72df8223c1de1f
Randomnickname12 <eliquinoxltd@gmail.com>
2024-06-28 20:55:11 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import testRoute from "./routes/test.route.js";
+
 import userRoute from "./routes/user.route.js";
 
 const app = express();
@@ -13,7 +13,6 @@ app.use(cookieParser());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
-app.use("/api/test", testRoute);
 app.use("/api/user", userRoute);
 
 app.listen(8800, () => {
