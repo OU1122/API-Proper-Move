17f1a4ae7136494e20e01edbc870e1de122c22fb
Randomnickname12 <eliquinoxltd@gmail.com>
2024-07-03 20:12:47 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -63,7 +63,7 @@ export const login = async (req, res) => {
 		res.cookie("token", token, {
 			httpOnly: true,
 			maxAge: age,
-			secure,
+			secure: true,
 			sameSite: "Strict",
 		})
 			.status(200)
