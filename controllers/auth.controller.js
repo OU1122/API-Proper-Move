de6d4bfc0db4e7935609771ca150e356d16e9388
Randomnickname12 <eliquinoxltd@gmail.com>
2024-07-04 13:47:13 +0200

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
 			httpOnly: true,
 			maxAge: age,
 			secure: true,
-			sameSite: "Lax",
+			sameSite: "None",
 		})
 			.status(200)
 			.json(userData);
